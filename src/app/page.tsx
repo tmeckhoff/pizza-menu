f83e6b3b4e36675fe5638464d11a81f1eb3cf79c
Tara Eckhoff <newberrytara30@gmail.com>
2026-02-03 13:35:52 -0600

    feat: modify home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,10 @@ export default async function Home() {
   return (
     <main className="m-6">
       <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl mb-20">My Pizzas</h1>
+        <h1 className="text-2xl mb-20">Tara's Pizzas</h1>
+      </div>
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-2xl mb-6 text-slate-800">Menu</h2>
       </div>
       <PizzaMenu pizzaData={pizzaData} />
     </main>
